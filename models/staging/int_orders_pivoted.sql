433eda8333e4a02eeae915c8997b98936fbc7909
elieruban <elie.ruban@artefact.com>
2026-02-24 10:14:53 -0500

    added jinja for loop

--- models/staging/int_orders_pivoted.sql
+++ models/staging/int_orders_pivoted.sql
@@ -6,10 +6,15 @@ with payments as
 , pivoted as (
     select 
         order_id,
-        {% set payment_methods=['bank_transfer', 'coupon', 'credit_card', 'gift_card']%}
+        {%- set payment_methods=['bank_transfer', 'coupon', 'credit_card', 'gift_card']-%}
 
-        {% for method in payment_methods%}
-            sum(case when payment_method = '{{ method }}' then amount else 0 end) as {{ method }}_amount,
+        {% for method in payment_methods %}
+            
+            sum(case when payment_method = '{{ method }}' then amount else 0 end) as {{ method }}_amount
+            {%- if not loop.last -%}
+                ,
+            {%- endif -%}
+
         {% endfor %}
     from payments
     group by 1
