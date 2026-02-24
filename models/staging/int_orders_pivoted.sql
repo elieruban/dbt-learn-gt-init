a718db840b9d9f5d53370aa29d4d78e48cecdd87
elieruban <elie.ruban@artefact.com>
2026-02-24 10:06:35 -0500

    jinja orders pivoted

---
+++ models/staging/int_orders_pivoted.sql
@@ -0,0 +1,17 @@
+with payments as 
+(
+    select * from {{ ref('stg_stripe_payments')}}
+    where status = 'success'
+)
+, pivoted as (
+    select 
+        order_id,
+        {% set payment_methods=['bank_transfer', 'coupon', 'credit_card', 'gift_card']%}
+
+        {% for method in payment_methods%}
+            sum(case when payment_method = '{{ method }}' then amount else 0 end) as {{ method }}_amount,
+        {% endfor %}
+    from payments
+    group by 1
+)
+select * from pivoted
